82450e6c2b8f92c193e9a06cf9d6342c1db9baf5
FinnT730 <FinnT730@gmail.com>
2022-09-06 18:33:46 +0200

    V0.0.1

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Movie } from './movie';
 
 @Component({
   selector: 'app-folder',
@@ -7,25 +8,33 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 
-class Movie {
-
-  public name: string;
-  public gerene: string;
-  public imgUrl: string;
-    
-}
-
 
 export class FolderPage implements OnInit {
-  public folder: string;
 
-  
-  public movies: Movie[];
+  public folder: string;
+  public movies: Movie[]= [];
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+    if(this.folder == "Finn") {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=7d87b01406d2be659dd0cb0017acf2db')
+    .then((response) => response.json())
+    .then((data) => {
+      let dat = data;
+      console.log(dat);
+        for(let movie of dat.results) {
+          // console.log(movie.title);
+          let _m = new Movie();
+          _m.name = movie.title;
+          _m.adult = movie.adult;
+          _m.desc = movie.overview;
+          this.movies.push(_m);
+        }
+    });
+  }
+
   }
 
 }
---
+++ src/app/folder/movie.ts
@@ -0,0 +1,8 @@
+export class Movie {
+    public name: string;
+    public adult: boolean;
+    public desc: string;
+    public imgPath: string;
+    public popularity: number;
+    public genre_ids: number[];
+}
