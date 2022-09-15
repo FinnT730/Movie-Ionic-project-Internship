1734bce0749c94d40c0a64583582f95905b67c01
FinnT730 <FinnT730@gmail.com>
2022-09-15 10:33:54 +0200

    Youtube embeds are now working. It is now using Youtube pipes.

--- src/app/description-of-movie/description-of-movie.module.ts
+++ src/app/description-of-movie/description-of-movie.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DescriptionOfMoviePageRoutingModule } from './description-of-movie-routing.module';
 
 import { DescriptionOfMoviePage } from './description-of-movie.page';
+import { YoutubeEmbedPipe } from './youtube-embed.pipe';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { DescriptionOfMoviePage } from './description-of-movie.page';
     IonicModule,
     DescriptionOfMoviePageRoutingModule
   ],
-  declarations: [DescriptionOfMoviePage]
+  declarations: [DescriptionOfMoviePage, YoutubeEmbedPipe]
 })
 export class DescriptionOfMoviePageModule {}
